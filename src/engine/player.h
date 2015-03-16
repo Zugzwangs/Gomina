4e63b45aea515c544ac6e99c9ea1f0b2e11cfbf0
jr <jr_belin@hotmail.com>
2015-03-16 22:48:17 +0100

    def de la classe abstraite player

--- src/engine/player.h
+++ src/engine/player.h
@@ -11,12 +11,35 @@
 class Player : public QObject
 {
     Q_OBJECT
+    Q_ENUMS(Location)
+    Q_ENUMS(Status)
+    Q_ENUMS(Side)
 
 public:
     explicit Player(QObject *parent = 0);
+    virtual void init(Location Loc) = 0;
     ~Player();
-    virtual void init() = 0;
 
+    enum Location { Gui = 0, Network = 1, Brain = 2 };
+    enum Status { Active = 0, Defend = 1, Spectate = 2, Wait = 3 };
+    enum Side { Black = 0, White = 1, NoSide = 2 };
+
+    void setName(QString _name);
+    void setAvatar(QString _avatar);
+    void setStatus(Status _stat);
+    void setRole(Side _side);
+
+private:
+    QString name;
+    QString avatar;
+    int rank;
+    int elo;
+    Status stat;
+    Location playFrom;
+    Side role;
+
+signals:
+    void loaded();
 
 };
 
